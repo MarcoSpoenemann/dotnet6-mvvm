172e665bcb31c51a2b61b57bba4c2c633f612709
Marco Spönemann <m.spoenemann@itwh.de>
2022-07-13 13:22:44 +0200

    Initial bug

---
+++ src/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace mvvm_bug.ViewModel;
+
+public partial class MainWindowViewModel : ObservableObject
+{
+
+    [ObservableProperty]
+    private string _someString;
+
+
+    public MainWindowViewModel()
+    {
+        _someString = "DefaultText";
+    }
+}
